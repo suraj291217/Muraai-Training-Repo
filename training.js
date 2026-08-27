48de4264d92d7b2569e4d1586840fbedf1b50d3d
Suraj Jagadeesh <surajjagadeesh1@gmail.com>
2026-08-27 17:33:03 +0530

    Add javascript operators

---
+++ training.js
@@ -0,0 +1,63 @@
+// let price = 500;
+// let quantity = 3;
+
+// let total = price * quantity;
+// console.log(total);
+
+
+// let number = 20;
+
+// if(number%2==0) {
+//     console.log("even");
+// }
+
+// let age = 20;
+
+// if(age>18) {
+//     console.log("he can drive");
+// } else {
+//     console.log("cannot drive");
+// }
+
+// let balance = 100000;
+
+// balance -= 50000;
+
+// console.log(balance);
+
+// let name = "Suraj";
+// console.log(typeof name);
+
+// let isStudent = true;
+// console.log(typeof isStudent);
+
+// let age = 20;
+
+// if(typeof age == "number") {
+//     console.log("yes it is");
+// }
+
+let price = 2000;
+let quantity = '2';
+let discount = 500;
+
+console.log(price + quantity);
+
+let total = price * quantity;
+
+total -= discount;
+
+let isMember = true;
+let minimumAmount = 3000;
+
+if (total >= minimumAmount && isMember) {
+    console.log("Discount applicable");
+} else {
+    console.log("Discount not applicable");
+}
+
+console.log("Final Amount:", total);
+
+
+
+
