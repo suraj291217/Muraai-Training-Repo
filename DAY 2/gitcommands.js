3d2688936e089640cfaf388718a9568e9fd0c7b5
Suraj Jagadeesh <surajjagadeesh1@gmail.com>
2026-08-31 12:11:31 +0530

    Add Day 2 JavaScript practice

---
+++ DAY 2/gitcommands.js
@@ -0,0 +1,51 @@
+GITHUB COMMANDS
+
+1. Check Git version
+   git --version
+
+2. Initialize Git repository
+   git init
+
+3. Check status
+   git status
+
+4. Add files
+   git add .
+
+5. Commit changes
+   git commit -m "message"
+
+6. Connect to GitHub
+   git remote add origin <repository-url>
+
+7. Push code to GitHub
+   git push -u origin main
+
+8. Get latest code from GitHub
+   git pull
+
+BASIC LINUX COMMANDS
+
+1. Show current directory
+   pwd
+
+2. List files and folders
+   ls
+
+3. Change directory
+   cd folder-name
+
+4. Create a folder
+   mkdir folder-name
+
+5. Create a file
+   touch file-name.txt
+
+BASIC GIT WORKFLOW
+
+git init
+git status
+git add .
+git commit -m "Initial commit"
+git remote add origin <repository-url>
+git push -u origin main
---
+++ DAY 2/script.js
@@ -0,0 +1,10 @@
+// let students = ["Rahul", "Suraj", "Anu", "Priya"];
+
+// students.forEach(student => {
+//     console.log(student);
+// });
+
+let prices = [100, 200, 300];
+
+let priceWithGST = prices.map(price => price * 1.18);
+console.log(priceWithGST);
